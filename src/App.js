f1d201f3627fb3fb4f09e865e774d741c635ad03
Dan Ott <thedanielott@gmail.com>
2019-11-02 15:09:30 -0400

    💾 Search for passages and save them to local storage

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import useLocalStorage from "./hooks/useLocalStorage";
+
+import "./App.css";
 
 function App() {
+  const [passages, setPassages] = useLocalStorage("passages", []);
+  const [selected, setSelected] = useLocalStorage("selected", null)
+
+  function searchForPassage(passage) {
+    const q = prompt("Search for a passage");
+    if (q) {
+      fetch(`/.netlify/functions/search?q=${q}`)
+        .then(r => r.json())
+        .then(payload => {
+          console.log("appendPassage", payload);
+          setPassages([...passages, ...payload.passages]);
+        })
+        .catch(error => {
+          console.log(error);
+          alert("Something went wrong");
+        });
+    }
+  }
+
+  const enriched = passages.map(p => {
+    let [title, ...passage] = p.split("\n");
+    passage = passage.join("\n");
+
+    return { title, passage };
+  });
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={searchForPassage}>➕Add passage</button>
+      {passages.length === 0 ? (
+        <div>There are no passages</div>
+      ) : (
+        <ul>
+          {enriched.map(({ title }) => (
+            <li key={title}>{title}</li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
